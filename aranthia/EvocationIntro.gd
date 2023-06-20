7c19e3ae5af9597d558dda70b35d5f353d378175
Christos Georgiou <christaras.georgiou@gmail.com>
2023-06-21 00:16:42 +0300

    Evocation Intro and Scene, Transmutation Intro and Scene

---
+++ aranthia/EvocationIntro.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var timeline = Dialogic.start("timeline-1685557697.json")
+	add_child(timeline)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
